970e5b8f2d351de28118bc4f9f0d83db39d258da
sylvanaar <sylvanaar@3720221d-9554-4cde-85f8-186c078cf8fe>
2008-10-21 10:47:33 +0000

    api outline

---
+++ trunk/LibAlts-1.0.lua
@@ -0,0 +1,30 @@
+﻿--[[
+Name: LibAlts-1.0
+Revision: 1
+Author: Sylvanaar (sylvanaar@mindspring.com)
+Description: Shared handling of alt identity between addons.
+Dependencies: LibStub
+License: 
+]]
+
+local MAJOR, MINOR = "LibAlts-1.0", "1"
+local lib = LibStub:NewLibrary(MAJOR, MINOR)
+if not lib then return end
+
+local _G = getfenv(0)
+
+function lib:Register(main, alt)
+end
+
+function lib:GetAlts(main)
+end
+
+function lib:GetMain(alt)
+end
+
+function lib:IsMain(main)
+end
+
+function lib:IsAlt(alt)
+end
+
