d03f5a09f44267ccbcedba7f3e5fc7db64184f40
sylvanaar <sylvanaar@3720221d-9554-4cde-85f8-186c078cf8fe>
2009-09-07 18:34:49 +0000

    patch contributed by orionshock - fixes upgrading issues, fixes bug in the return value of getallmains()

--- trunk/LibAlts-1.0.lua
+++ trunk/LibAlts-1.0.lua
@@ -21,9 +21,12 @@ local Mains  -- reverse lookup table
 
 local tinsert = _G.tinsert
 local unpack = _G.unpack
+local pairs = _G.pairs
+local tremove = _G.tremove
+local tsort = _G.table.sort
 
-
-local callbacks = LibStub("CallbackHandler-1.0"):New(lib)
+lib.callbacks = lib.callbacks or LibStub("CallbackHandler-1.0"):New(lib)
+local callbacks = lib.callbacks
 
 local function generateRevLookups()
 	Mains = {}
@@ -82,17 +85,27 @@ function lib:GetMain(alt)
 
 	return Mains[alt]
 end
-
-
+local unpackDictionary
+do
+	local temp = {}
+	function unpackDictionary(t)
+		for i = 1, #temp do temp[i] = nil end
+		for k,v in pairs(t) do
+			tinsert(temp, k)
+		end
+		tsort(temp)
+		return unpack(temp)
+	end
+end
 --- Get a list of all main characters
--- @name :GetAllMains() 
+-- @name :GetAllMains
 -- @return list of all main characters 
 function lib:GetAllMains()
 	if not Mains then
 		generateRevLookups()
 	end
 
-	return unpack(Mains)
+	return unpackDictionary(Alts)
 end
 
 
@@ -120,3 +133,21 @@ function lib:IsAlt(alt)
 	return Mains[alt:lower()] and true or false
 end
 
+--- Remove an Main Alt relationship and fire a callback for the disassociation.
+-- @name :DeleteAlt
+-- @param main Name of the Main character
+-- @param alt Name of the Alt being removed
+function lib:DeleteAlt(main, alt)
+	main, alt = main:lower(), alt:lower()
+	if not Alts[main] then return end
+	Mains = nil
+	for i = 1, #Alts[main] do
+		if Alts[main][i] == alt then
+			tremove(Alts[main], i)
+		end
+	end
+	if #Alts[main] == 0 then
+		Alts[main] = nil
+	end
+	callbacks:Fire("LibAlts_RemoveAlt", main, alt)	--Alt is the one removed
+end
